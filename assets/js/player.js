21102c0fb3b89d6686a7e5e4a3dfabd4292b615b
Justin VanHaven <vanhavenjustin@gmail.com>
2022-01-09 20:27:33 -0500

    added audiobook files

--- assets/js/player.js
+++ assets/js/player.js
@@ -12,7 +12,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ['hey', 'summer', 'ratrace'];
+const songs = ['Podcast', 'Narration-NonFiction', 'MultiVoice', 'Narration-Fiction'];
 
 // Keep track of song
 let songIndex = 2;
